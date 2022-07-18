9f1fc201cf234d54bac484bb6385c190c3e464cf
chris-netizen <adedejisomoye1@gmail.com>
2022-07-18 10:50:41 +0000

    My First Commit

---
+++ lib/auth/screens/homepage.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
