79e650d0b0ceb5896bd2538c8e3b29fc0484c049
Micheal <aokhaimoh@yahoo.com>
2022-07-23 22:05:00 +0100

    implemented login and signup with firebase

--- lib/views/screens/homepage.dart
+++ lib/views/screens/homepage.dart
@@ -1,8 +1,4 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
-
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
